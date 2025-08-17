54051f481fb8721c0e7e0b6c8ab895184a648c19
Nuruzzaman-Nuru <nnuru8805@gmail.com>
2025-08-18 00:08:43 +0600

    a

--- run.py
+++ run.py
@@ -1,11 +1,16 @@
 from ecommerce import create_app
 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello from QuickShop!'
+
+# Import and initialize the main application
 from ecommerce import create_app
-
 app = create_app()
-
-# Vercel needs this variable to be named 'app'
-application = app
 
 if __name__ == '__main__':
     app.run()
