de4b589c3f0db7c0b522f61fd437c703d1404c8f
Nuruzzaman-Nuru <nnuru8805@gmail.com>
2025-08-18 01:27:13 +0600

    Simplify Flask setup for Vercel

--- api/index.py
+++ api/index.py
@@ -1,6 +1,11 @@
 from flask import Flask
-from app import app
 
-# For Vercel Serverless Function
-def handler(request):
-    return app
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello from QuickShop!'
+
+@app.route('/api/test')
+def test():
+    return {"message": "API is working!"}
